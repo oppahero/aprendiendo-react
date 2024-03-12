5db928de67dfcf1b4830996f17fab1d6e8c11f2e
María López <37983313+oppahero@users.noreply.github.com>
2024-03-12 13:36:18 -0400

    prop drilling & useContext

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,40 +1,23 @@
 import './App.css'
 import { useState } from 'react'
+import { IS_DEVELOPMENT } from './config.js'
 import { Products } from './components/Products.jsx'
 import { Header } from './components/Header.jsx'
 import { Footer } from './components/Footer.jsx'
 import { products as initialProducts } from './mocks/products.json'
-import { IS_DEVELOPMENT } from './config.js'
-
-function useFilters () {
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return product.price >= filters.minPrice && (
-        filters.category === 'all' ||
-        product.category === filters.category
-      )
-    })
-  }
-
-  return { filters, filterProducts, setFilters }
-}
+import { useFilters } from './hooks/useFilters.js'
 
 function App () {
   const [products] = useState(initialProducts)
-  const { filters, filterProducts, setFilters } = useFilters()
+  const { filterProducts } = useFilters()
 
   const filteredProducts = filterProducts(products)
 
   return (
     <>
-      <Header changeFilters={setFilters} />
+      <Header />
       <Products products={filteredProducts} />
-      {IS_DEVELOPMENT && <Footer filters={filters} />}
+      {IS_DEVELOPMENT && <Footer />}
     </>
   )
 }
--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,15 +1,15 @@
-import { useState, useId } from 'react'
+import { useId } from 'react'
 import './Filters.css'
+import { useFilters } from '../hooks/useFilters'
 
-export function Filters ({ onChange }) {
-  const [minPrice, setMinPrice] = useState(0)
+export function Filters () {
+  const { filters, setFilters } = useFilters()
+
   const minPriceFilterId = useId()
   const categoryFilterId = useId()
 
   const handleChangeMinPrice = (event) => {
-    // !Algo huele mal, dos fuentes de la verdad
-    setMinPrice(event.target.value)
-    onChange(prevState => ({
+    setFilters(prevState => ({
       ...prevState,
       minPrice: event.target.value
     }))
@@ -17,7 +17,7 @@ export function Filters ({ onChange }) {
 
   const handleChangeCategory = (event) => {
     // !Algo huele mal
-    onChange(prevState => ({
+    setFilters(prevState => ({
       ...prevState,
       category: event.target.value
     }))
@@ -33,8 +33,9 @@ export function Filters ({ onChange }) {
           min='0'
           max='1000'
           onChange={handleChangeMinPrice}
+          value={filters.minPrice}
         />
-        <span>${minPrice}</span>
+        <span>${filters.minPrice}</span>
       </div>
       <div>
         <label htmlFor={categoryFilterId}>Categoría</label>
