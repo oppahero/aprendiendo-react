ccc54dddc858745801092b34ffb069e8af4ba979
Maria <you@example.com>
2024-03-15 13:19:41 -0400

    useReducer

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -1,40 +1,72 @@
-import { createContext, useState } from 'react'
+import { createContext, useReducer } from 'react'
+import { cartInitialState, cartReducer } from '../reducers/cart.js'
 
 export const CartContext = createContext()
 
+function useCartReducer () {
+  const [state, dispatch] = useReducer(cartReducer, cartInitialState)
+
+  const addToCart = (product) => dispatch({
+    type: 'ADD_TO_CART',
+    payload: product
+  })
+
+  const removeFromCart = (product) => dispatch({
+    type: 'REMOVE_FROM_CART',
+    payload: product
+  })
+
+  const clearCart = () => dispatch({
+    type: 'CLEAR_CART'
+  })
+
+  return {
+    state,
+    addToCart,
+    removeFromCart,
+    clearCart
+  }
+}
+
 export function CartProvider ({ children }) {
-  const [cart, setCart] = useState([])
+  /* antes de usar un reducer
 
-  const addToCart = product => {
-    const productInCartIndex = cart.findIndex(item => item.id === product.id)
+    const [cart, setCart] = useState([])
 
-    if (productInCartIndex >= 0) {
-      const newCart = structuredClone(cart)
-      newCart[productInCartIndex].quantity++
-      return setCart(newCart)
+    const addToCart = product => {
+      const productInCartIndex = cart.findIndex(item => item.id === product.id)
+
+      if (productInCartIndex >= 0) {
+        const newCart = structuredClone(cart)
+        newCart[productInCartIndex].quantity++
+        return setCart(newCart)
+      }
+
+      setCart(prevState => (
+        [
+          ...prevState,
+          {
+            ...product,
+            quantity: 1
+          }
+        ]
+      ))
     }
 
-    setCart(prevState => (
-      [
-        ...prevState,
-        {
-          ...product,
-          quantity: 1
-        }
-      ]
-    ))
-  }
+    const clearCart = () => {
+      setCart([])
+    }
 
-  const clearCart = () => {
-    setCart([])
-  }
+    const removeFromCart = product => {
+      setCart(prevState => prevState.filter(item => item.id !== product.id))
+    }
 
-  const removeFromCart = product => {
-    setCart(prevState => prevState.filter(item => item.id !== product.id))
-  }
+  */
+
+  const { state, addToCart, removeFromCart, clearCart } = useCartReducer()
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, clearCart, removeFromCart }}>
+    <CartContext.Provider value={{ cart: state, addToCart, clearCart, removeFromCart }}>
       {children}
     </CartContext.Provider>
   )
