1630bd80e075d25a9d134b4885168802f12977a2
María López <37983313+oppahero@users.noreply.github.com>
2024-03-20 14:14:28 -0400

    Lazy loading, suspense

--- projects/07-midu-router/src/App.jsx
+++ projects/07-midu-router/src/App.jsx
@@ -1,26 +1,42 @@
+// imports estáticos
 import './App.css'
-import HomePage from './pages/Home.jsx'
-import AboutPage from './pages/About.jsx'
-import Page404 from './pages/404.jsx'
-import SearchPage from './pages/SearchPage.jsx'
+import { Suspense, lazy } from 'react'
 import { Router } from './Router.jsx'
 import { Route } from './Route.jsx'
 
+// ?Imports dinámicos: devuelven una promesa
+// lazy recibe una función que hará el import
+const LazyAboutPage = lazy(() => import('./pages/About.jsx'))
+const LazyHomePage = lazy(() => import('./pages/Home.jsx'))
+const Lazy404Page = lazy(() => import('./pages/404.jsx'))
+const LazySearchPage = lazy(() => import('./pages/Search.jsx'))
+
 const appRoutes = [
   {
+    path: '/:lang/about',
+    Component: LazyAboutPage
+  },
+  {
     path: '/search/:query',
-    Component: SearchPage
+    Component: LazySearchPage
   }
 ]
+
+// Cuando se haga imports dinámicos es necesario decirle a react que hay elementos de ui que no estaán disponibles
+// Para eso hay que envolver esa parte con las etiquetas <Suspense></Suspense>
+// Suspense puede recibir un callback, si hay una parte de Ui que está disponible}
+// se podria decirle que poner mientras
 
 function App () {
   return (
     <>
       <main>
-        <Router routes={appRoutes} defaultComponent={Page404}>
-          <Route path='/' Component={HomePage} />
-          <Route path='/about' Component={AboutPage} />
-        </Router>
+        <Suspense fallback={<div>Loading</div>}>
+          <Router routes={appRoutes} defaultComponent={Lazy404Page}>
+            <Route path='/' Component={LazyHomePage} />
+            <Route path='/about' Component={LazyAboutPage} />
+          </Router>
+        </Suspense>
       </main>
     </>
   )
--- projects/07-midu-router/src/pages/Search.jsx
+++ projects/07-midu-router/src/pages/Search.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 
-export default function SearchPage ({ routeParams }) {
+export default function Search ({ routeParams }) {
   useEffect(() => {
     document.title = `Has buscado ${routeParams.query}`
   }, [])
