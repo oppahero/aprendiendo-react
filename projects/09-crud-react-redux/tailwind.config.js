546dd57eefea1f3566f1b7a170187ae9140e137a
Maria <you@example.com>
2024-03-30 17:59:32 -0400

    Tailwind comfig

---
+++ projects/09-crud-react-redux/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+
+    // Path to Tremor module
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
