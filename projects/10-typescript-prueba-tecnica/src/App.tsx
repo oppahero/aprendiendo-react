1176f8633406af71ae48bdfe783013b3ff815245
María López <37983313+oppahero@users.noreply.github.com>
2024-04-02 13:30:03 -0400

    add new proyect

---
+++ projects/10-typescript-prueba-tecnica/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function App () {
+  return (
+    <>
+      <div >
+        <h1>Prueba Técnica Typescript</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
